548d16198427bce6ced41905fae94043f0f66904
sayanta1999 <47280553+sayanta1999@users.noreply.github.com>
2019-04-27 00:03:36 +0530

    Add files via upload

---
+++ Breast_cancer_type.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 22 09:46:19 2019
+
+@author: KIIT
+"""
+
+import pandas as pd
+from sklearn.utils import shuffle
+from sklearn.cross_validation import train_test_split
+from sklearn.metrics import accuracy_score,confusion_matrix
+import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestClassifier
+
+def read_data():
+    df = pd.read_csv('data.csv')
+    df=df.fillna(0)
+    x = df.drop('diagnosis',axis=1)
+    y = df['diagnosis']
+    x,y = shuffle(x,y,random_state = 1)
+    return x,y
+
+def model_rfc(x_train,y_train):
+    num_trees = 31
+    max_features = 3
+    rfc = RandomForestClassifier(n_estimators = num_trees,max_features = max_features, random_state = 5,n_jobs = 2)
+    rfc.fit(x_train,y_train)
+    return rfc
+
+def main():
+    x,y = read_data()
+    x_train,x_test,y_train,y_test = train_test_split(x,y,test_size = 0.3,random_state=7)
+    
+    rfc = model_rfc(x_train,y_train)
+    predictions = rfc.predict(x_test)
+    print("random forest results : ",accuracy_score(predictions,y_test))
+    
+    print("Confusion Matrix")
+    print(confusion_matrix(predictions,y_test))
+    
+    plt.plot(predictions,y_test)
+    plt.xlabel('Predicted Result')
+    plt.ylabel('Actual Result')
+    plt.show()
+    
+if __name__ == '__main__':
+    main()
